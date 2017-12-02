af8313dcb59ad47dcab276045a8ac84fdd527b2a
oti_i <oti_i@172.28.0.78>
2017-12-02 00:46:43 -0400

    Ignore some files

--- includes/footer.php
+++ includes/footer.php
@@ -10,7 +10,7 @@
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- DataTables JavaScript -->
+    <!-- DataTables JavaScript:  https://datatables.net/manual/installation -->
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
     <script>
